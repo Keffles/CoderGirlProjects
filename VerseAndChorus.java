ccf9c909deb897fc179ce11ab1c19033b665ef36
Keffles <46980756+Keffles@users.noreply.github.com>
2019-02-12 21:23:41 -0600

    Verse and Chorus Exercise
    Exercise using defining and calling a method or function.

---
+++ VerseAndChorus.java
@@ -0,0 +1,41 @@
+import static java.lang.System.out;
+
+
+public class VerseAndChorus {
+    public static void PaddyWhack()
+    {
+        out.println("With a knick-knack paddywhack,");
+        out.println("Give the dog a bone,");
+        out.println("This old man came rolling home.");
+        out.println();
+    }
+
+    public static void main(String[] args)
+    {
+        out.println("This old man, he played one.");
+        out.println("He played knick-knack on my thumb;");
+        PaddyWhack();
+
+        out.println("This old man, he played two.");
+        out.println("He played knick-knack on my shoe;");
+        PaddyWhack();
+
+        out.println("This old man, he played three;");
+        out.println("He played knick-knack on my knee");
+        PaddyWhack();
+
+        out.println("This old man, he played four.");
+        out.println("He played knick-knack on my door");
+        PaddyWhack();
+
+        out.println("This old man, he played five.");
+        out.println("He played knick-knack on my hive;");
+        PaddyWhack();
+
+        out.println("This old man, he played six.");
+        out.println("He played knick-knack on my sticks");
+        PaddyWhack();
+
+
+    }
+}
